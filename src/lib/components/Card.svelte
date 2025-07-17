5e4f7474508cd090c4d0d426473eb731a3070a81
Gabriel-RQ <123425138+Gabriel-RQ@users.noreply.github.com>
2025-07-17 09:09:34 -0300

    feat: card component

---
+++ src/lib/components/Card.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import type { Snippet } from "svelte";
+
+  type CardProps = {
+    children: Snippet;
+  };
+
+  let { children }: CardProps = $props();
+</script>
+
+<div
+  class="glass flex flex-col gap-2 items-center justify-center rounded-lg p-2"
+>
+  {@render children()}
+</div>
--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -1,1 +1,2 @@
 export { default as Header } from "./Header.svelte";
+export { default as Card } from "./Card.svelte";
